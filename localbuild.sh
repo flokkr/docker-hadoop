4b335d8347850ed598466a09531824428b48f519
Elek, Márton <elek@users.noreply.github.com>
2016-11-05 10:23:55 +0100

    script to build images locally

---
+++ localbuild.sh
@@ -0,0 +1,4 @@
+sudo docker build -t elek/hadoop-hdfs-datanode hdfs-datanode
+sudo docker build -t elek/hadoop-hdfs-namenode hdfs-namenode
+sudo docker build -t elek/hadoop-yarn-nodemanager yarn-nodemanager
+sudo docker build -t elek/hadoop-yarn-resourcemanager yarn-resourcemanager
