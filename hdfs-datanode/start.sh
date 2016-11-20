55e6d87b2807899e3a9fd53931ac27fe87ca36ff
Elek, Márton <elek@users.noreply.github.com>
2016-11-20 18:42:07 +0100

    switch to the new configuration loading

--- hdfs-datanode/start.sh
+++ hdfs-datanode/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 set -x
+export CONSUL_LOCATION=172.17.0.1:8500
+export CONFIG_SERVER_URL=http://172.17.0.1:8888
+export RUNTIME_ENV=dev
+export CONF_DIR=/usr/hdp/hadoop/etc/hadoop
 source /usr/hdp/hadoop-configurator.sh
-configure /usr/hdp/hadoop/etc/hadoop/core-site CORE_SITE 
-configure /usr/hdp/hadoop/etc/hadoop/hdfs-site HDFS_SITE 
+configure core-site
+configure hdfs-site
 /usr/hdp/hadoop/bin/hdfs datanode
