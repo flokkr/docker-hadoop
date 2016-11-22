827d5eb9436992c866a8173b080d5f94c14cd034
Elek, Márton <elek@users.noreply.github.com>
2016-11-22 20:58:07 +0100

    fix hdfs-datanode starter script

--- hdfs-datanode/start.sh
+++ hdfs-datanode/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 set -x
-export CONSUL_LOCATION=172.17.0.1:8500
-export CONFIG_SERVER_URL=http://172.17.0.1:8888
-export RUNTIME_ENV=dev
 export CONF_DIR=/usr/hdp/hadoop/etc/hadoop
 source /usr/hdp/hadoop-configurator.sh
 configure core-site
