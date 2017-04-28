7ce25446aa518956ac241b6b0d9f1caa4c7299dd
Elek, Márton <elek@users.noreply.github.com>
2017-04-28 21:59:11 +0200

    add merger and branch refresh scripts

--- branch.sh
+++ branch.sh
@@ -4,4 +4,3 @@ URL=$2
 BASE_IMAGE=$(basename $(dirname $(find -name baseimage)))
 echo $URL > $BASE_IMAGE/url
 grep -l -r "$BASE_IMAGE:latest" | grep -v branch.sh |xargs sed -i "s/$BASE_IMAGE:latest/$BASE_IMAGE:$TAG/g"
-./localbuild.sh $TAG
