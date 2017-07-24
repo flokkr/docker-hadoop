37b95d64652c174340f0c334fa3a3fa499781863
Elek, Márton <elek@users.noreply.github.com>
2017-07-24 22:28:59 +0200

    remove branch.sh

--- branch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TAG=${1:-latest}
-URL=$2
-BASE_IMAGE=$(basename $(dirname $(find -name baseimage)))
-echo $URL > $BASE_IMAGE/url
-grep -l -r "$BASE_IMAGE:latest" | grep -v branch.sh |xargs sed -i "s/$BASE_IMAGE:latest/$BASE_IMAGE:$TAG/g"
