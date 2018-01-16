2cfd6f23f185d57aa779e5962470069e1cb6a179
Márton Elek <elek@apache.org>
2018-01-16 12:27:00 +0100

    support custom clusterid for namenode

--- runner/012_namenodeformat/namenodeformat.sh
+++ runner/012_namenodeformat/namenodeformat.sh
@@ -1,10 +1,12 @@
 #/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 if [ -n "$ENSURE_NAMENODE_DIR" ]; then
+   CLUSTERID_OPTS=""
+   if [ -n "$ENSURE_NAMENODE_CLUSTERID" ]; then
+      CLUSTERID_OPTS="-clusterid $ENSURE_NAMENODE_CLUSTERID"
+   fi
    if [ ! -d "$ENSURE_NAMENODE_DIR" ]; then
-      /opt/hadoop/bin/hdfs namenode -format
+      /opt/hadoop/bin/hdfs namenode -format $CLUSTERID_OPTS
         fi
 fi
-
 call-next-plugin "$@"
