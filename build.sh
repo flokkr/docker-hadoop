13adae0bbc383e2b3484560cc165517b4ae78ca8
Márton Elek <elek@apache.org>
2018-04-10 00:28:26 +0200

    switch to shell based build script with docker label support

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -x 
+
+build() {
+	echo ${URL} > hadoop/url
+	docker build --label io.github.flokkr.hadoop.version=$HADOOP_VERSION -t flokkr/hadoop-runner:${TAG} runner
+	docker tag flokkr/hadoop-runner:${TAG} flokkr/hadoop-runner:build
+	docker build --label io.github.flokkr.hadoop.version=$HADOOP_VERSION -t flokkr/hadoop:${TAG} hadoop
+}
+
+deploy() {
+	docker push flokkr/hadoop-runner:${TAG}
+	docker push flokkr/hadoop:${TAG}
+}
+
+TAG=latest
+HADOOP_VERSION=3.1.0
+
+while getopts ":v:t:" opt; do
+  case $opt in
+    v)
+      HADOOP_VERSION=$OPTARG >&2
+      ;;
+    t)
+      TAG=${OPTARG:-$HADOOP_VERSION}
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+export URL=https://www-eu.apache.org/dist/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz
+
+shift $(($OPTIND -1))
+eval $*
