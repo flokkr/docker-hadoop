00a8ba52be11e1df4d6ff0ab164ffd9c97116787
Márton Elek <elek@apache.org>
2018-04-21 21:18:38 +0200

    support custom namenode initialization command

--- runner/012_hdfsinit/hdfsinit.sh
+++ runner/012_hdfsinit/hdfsinit.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env bash
+set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -n "$ENSURE_NAMENODE_DIR" ]; then
    CLUSTERID_OPTS=""
@@ -7,7 +8,29 @@ if [ -n "$ENSURE_NAMENODE_DIR" ]; then
    fi
    if [ ! -d "$ENSURE_NAMENODE_DIR" ]; then
       /opt/hadoop/bin/hdfs namenode -format -force $CLUSTERID_OPTS
-        fi
+   fi
+   if [ "$NAMENODE_INIT" ]; then
+      hdfs namenode &
+		NNPID=$!
+      for i in `seq 30` ; do
+   
+		   NAMENODE_ADDRESS=$(hdfs getconf -confkey dfs.namenode.rpc-address)
+		   NN_WAITFOR_HOST=$(printf "%s\n" "$NAMENODE_ADDRESS"| cut -d : -f 1)
+         NN_WAITFOR_PORT=$(printf "%s\n" "$NAMENODE_ADDRESS"| cut -d : -f 2)
+         nc -z "$NN_WAITFOR_HOST" "$NN_WAITFOR_PORT" > /dev/null 2>&1
+
+         result=$?
+         if [ $result -eq 0 ] ; then
+            eval "$NAMENODE_INIT"
+            kill $NNPID
+				sleep 3
+            call-next-plugin "$@"  
+         fi     
+         sleep 1
+      done                          
+      echo "Operation timed out" >&2
+      exit 1
+   fi
 fi
 
 
