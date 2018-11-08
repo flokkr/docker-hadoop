be0357e2ec8f898e29487949abfee23cb8849226
Márton Elek <elek@apache.org>
2018-11-08 09:25:49 +0100

    improve build matrix calculation

--- build.sh
+++ build.sh
@@ -3,18 +3,29 @@
 set -e 
 
 build() {
-	echo ${URL} > hadoop/url
-	docker build --label io.github.flokkr.hadoop.version=$HADOOP_VERSION -t flokkr/hadoop-runner:${TAG} runner
-	docker tag flokkr/hadoop-runner:${TAG} flokkr/hadoop-runner:build
-	docker build --label io.github.flokkr.hadoop.version=$HADOOP_VERSION -t flokkr/hadoop:${TAG} hadoop
+   IFS=', ' read -r -a TAGA <<< "$TAGS"
+   TAG=${TAGA[0]}
+   echo ${URL} > hadoop/url
+   docker build --label io.github.flokkr.hadoop.version=$HADOOP_VERSION -t flokkr/hadoop-runner:${TAG} runner
+   docker tag flokkr/hadoop-runner:${TAG} flokkr/hadoop-runner:build
+   docker build --label io.github.flokkr.hadoop.version=$HADOOP_VERSION -t flokkr/hadoop:${TAG} hadoop
 }
 
 deploy() {
-	docker push flokkr/hadoop-runner:${TAG}
-	docker push flokkr/hadoop:${TAG}
+   IFS=', ' read -r -a TAGA <<< "$TAGS"
+   TAG=${TAGA[0]}
+   for CT in "${TAGA[@]}"
+   do
+      if [ "$TAG" != "$CT" ]; then
+         docker tag flokkr/hadoop-runner:${TAG} flokkr/hadoop-runner:${CT}
+         docker tag flokkr/hadoop:${TAG} flokkr/hadoop:${CT}
+      fi
+         docker push flokkr/hadoop-runner:${CT}
+         docker push flokkr/hadoop:${CT}
+   done
 }
 
-TAG=latest
+TAGS=latest
 HADOOP_VERSION=3.1.0
 
 while getopts ":v:t:" opt; do
@@ -23,7 +34,7 @@ while getopts ":v:t:" opt; do
       HADOOP_VERSION=$OPTARG >&2
       ;;
     t)
-      TAG=${OPTARG:-$HADOOP_VERSION}
+      TAGS=${OPTARG:-$HADOOP_VERSION}
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -35,6 +46,11 @@ if [ "$HADOOP_DOWNLOAD_URL" ]; then
   URL=$HADOOP_DOWNLOAD_URL
 else
   URL="https://www-eu.apache.org/dist/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz"
+  CHECK=$(curl -I HEAD $URL 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+  if [ $CHECK == "404" ]; then
+      echo "$URL is not found. Trying to use an archive link."
+      URL="https://archive.apache.org/dist/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz"
+  fi
 fi
 export URL
 shift $(($OPTIND -1))
