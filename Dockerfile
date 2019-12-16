e8b2591de19c584eae7bd3179ddd8f29cfa3aa0f
Márton Elek <elek@apache.org>
2019-12-17 00:55:42 +0100

    switch to the centos based base image with unified mage structure

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
-ARG BASE=33
+ARG BASE=latest
 FROM flokkr/base:${BASE}
 ARG URL
-RUN apk add --update --no-cache libstdc++ findutils ncurses && rm -rf /var/cache/apk/*
-RUN adduser -h /opt/ -s /bin/bash -G flokkr -D hadoop && chown hadoop /opt
+RUN useradd --uid 1000 hadoop --gid 1000 -G flokkr --home /opt/hadoop && chown hadoop /opt
 USER hadoop
 VOLUME /data
 RUN sudo chown hadoop /data
@@ -11,7 +10,8 @@ RUN sudo chmod o+x /opt/launcher/plugins/012_hdfsinit/hdfsinit.sh
 ENV CONF_DIR /opt/hadoop/etc/hadoop
 ENV PATH $PATH:/opt/hadoop/bin
 ENV JAVA_OPTS_VAR HADOOP_OPTS
-RUN wget $URL -O hadoop.tar.gz && tar zxf hadoop.tar.gz && rm hadoop.tar.gz && mv hadoop* hadoop && rm -rf /opt/hadoop/share/doc
+ADD --chown=hadoop:flokkr hadoop-3.2.1 /opt/hadoop
+#RUN wget $URL -O hadoop.tar.gz && tar zxf hadoop.tar.gz && rm hadoop.tar.gz && mv hadoop* hadoop && rm -rf /opt/hadoop/share/doc
 RUN chmod 775 /opt/hadoop/etc/hadoop
 WORKDIR /opt/hadoop
 ADD log4j.properties /opt/hadoop/etc/hadoop/log4j.properties
--- magefile.go
+++ magefile.go
@@ -3,83 +3,8 @@
 package main
 
 import (
-	"github.com/getlantern/errors"
-	"fmt"
-	"github.com/magefile/mage/sh"
-	"net/http"
+        // mage:import
+        _ "github.com/elek/magelib"
 )
 
-var name = "hadoop"
-var downloadPath = "hadoop/common/hadoop-%s/hadoop-%s.tar.gz"
 
-func versions() map[string][]string {
-	versions := make(map[string][]string)
-	versions["3.2.1"] = []string{"latest", "3.2.1", "3.2", "3"}
-//	versions["3.1.2"] = []string{"3.1.2", "3.1"}
-//	versions["3.1.1"] = []string{"3.1.1"}
-//	versions["3.1.0"] = []string{"3.1.0"}
-//	versions["3.0.3"] = []string{"3.0.3", "3.0"}
-	return versions
-}
-
-func buildImage(tag string, dir string, url string) {
-    sh.Run("docker", "build", "-t", tag, "--build-arg", "URL="+url, dir)
-}
-
-func getApacheDownloadUrl(path string) (string, error) {
-	url := fmt.Sprintf("https://www-eu.apache.org/dist/" + path)
-	resp, err := http.Head(url)
-	if err != nil {
-		return "", err
-	}
-	if resp.StatusCode == 200 {
-		return url, nil
-	}
-	url = fmt.Sprintf("https://archive.apache.org/dist/" + path)
-	resp, err = http.Head(url)
-	if err != nil {
-		return "", err
-	}
-	if resp.StatusCode == 200 {
-		return url, nil
-	}
-	return "", errors.New("Can't find download url for " + path)
-
-}
-func deployImage(tag string) {
-	sh.Run("docker", "push", tag)
-}
-
-func buildContainer(version string, tags []string) error {
-	url, err := getApacheDownloadUrl(fmt.Sprintf(downloadPath, version, version))
-	if err != nil {
-		return err
-	}
-	if err != nil {
-		return err
-	}
-	buildImage("flokkr/hadoop:build", ".", url)
-	for _, tag := range tags {
-		sh.Run("docker", "tag", "flokkr/hadoop:build", "flokkr/hadoop:"+tag)
-	}
-	return nil
-}
-
-func Build() error {
-	for version, tags := range versions() {
-		err := buildContainer(version, tags)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func Deploy() error {
-	for _, tags := range versions() {
-		for _, tag := range tags {
-			deployImage("flokkr/hadoop:" + tag)
-		}
-	}
-	return nil
-}
